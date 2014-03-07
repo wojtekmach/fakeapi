8ce24d080074baef8dd93f0d28e3dd5acb40af31
Wojtek Mach <wojtek@wojtekmach.pl>
2014-03-07 14:35:44 +0100

    Add data hash

--- lib/fakeapi/app.rb
+++ lib/fakeapi/app.rb
@@ -2,6 +2,13 @@ require 'sinatra/base'
 
 module FakeAPI
   class App < Sinatra::Base
+    attr_reader :data
+
+    def initialize(*)
+      super
+      @data = {}
+    end
+
     def self.test(method_name, &call_block)
       TestRun.new(self, method_name, call_block).run
     end
